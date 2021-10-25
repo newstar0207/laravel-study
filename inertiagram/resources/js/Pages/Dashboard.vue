ab1428e3a7be88923322f776d8a2838e570b406b
newstar <newstar0207@naver.com>
2021-10-25 19:31:43 +0900

    create sns service

---
+++ inertiagram/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <div v-if="$page.props.jetstream.managesProfilePhotos" class="flex-shrink-0 mr-3 px-40">
+                <img :src="$page.props.user.profile_photo_url" alt="username" class="rounded-full h-40 w-40 object-cover">
+            </div>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{  username }}
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden sm:rounded-lg">
+                    <post-list></post-list>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import PostList from './Instagram/PostList.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+            PostList,
+        },
+        props : [
+            'username'
+        ]
+    })
+</script>
---
+++ inertiagram/resources/js/Pages/Instagram/PostList.vue
@@ -0,0 +1,21 @@
+    <template>
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5" v-for="post in posts" :key="post.id">
+        <post-item></post-item>
+    </div>
+</template>
+<script>
+import PostItem from './PostItem.vue'
+export default {
+    components : {
+        PostItem,
+    },
+    data() {
+        return {
+            posts : [1,2,3,4,5]
+        }
+    }
+}
+</script>
+<style>
+
+</style>
